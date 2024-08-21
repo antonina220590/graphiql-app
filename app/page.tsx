9497ea69bec00118c89169b3357fcfccb7816cbb
antonina220590 <tonya2290@gmail.com>
2024-08-21 13:21:23 +0300

    feat: update lintstagedrc file settings

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   const b: number = 0;
   console.log(b);
   const a = 'a';
+  const c = 'c';
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
