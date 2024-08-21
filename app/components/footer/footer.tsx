a64688fe1d0e8976efa6ea9855d30779eecd6005
antonina220590 <tonya2290@gmail.com>
2024-08-21 21:55:44 +0300

    feat: delete ui folder

---
+++ app/components/footer/footer.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+
+import GithabIcon from '../../../public/github.svg';
+import RSSLogo from '../../../public/rss.svg';
+
+export default function Footer() {
+  return (
+    <footer className="bg-[#513125]">
+      <div className="p-5 flex justify-between items-center">
+        <Link href="https://rs.school/courses/reactjs" target="_blank">
+          <RSSLogo width="100" height="100" />
+        </Link>
+        <span className="text-light font-bold text-[45px]">2024</span>
+        <div className="flex">
+          <Link href="https://github.com/antonina220590" target="_blank">
+            <GithabIcon width="55" height="55" fill="#D9BCA9" />
+          </Link>
+          <Link href="https://github.com/inafk" target="_blank">
+            <GithabIcon width="55" height="55" fill="#D9BCA9" />
+          </Link>
+          <Link href="https://github.com/krkate" target="_blank">
+            <GithabIcon width="55" height="55" fill="#D9BCA9" />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+}
