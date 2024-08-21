2ae084fdce42238a93e3138a0c25af87aa97479f
antonina220590 <tonya2290@gmail.com>
2024-08-21 13:12:23 +0300

    feat: test husky pre-commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   const b: number = 0;
+  console.log(b);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
