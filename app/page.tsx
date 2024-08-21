1ae751159bf5ce7ef831f19dc0dd68c2cf880775
antonina220590 <tonya2290@gmail.com>
2024-08-21 13:15:49 +0300

    feat: test husky

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image';
 export default function Home() {
   const b: number = 0;
   console.log(b);
+  const a = 'a';
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
