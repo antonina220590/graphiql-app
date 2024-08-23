34e7658e41779169b6bb15805d2857b41d088942
Ekaterina Kiryanova <ekaterina.krashennikova@gmail.com>
2024-08-23 17:11:10 +0500

    feat: add buttons for home page

--- app/__tests__/page.test.tsx
+++ app/__tests__/page.test.tsx
@@ -5,5 +5,7 @@ import Page from '../page';
 
 test('Page', () => {
   render(<Page />);
-  expect(screen.getByRole('heading', { level: 1, name: 'Home' })).toBeDefined();
+  expect(
+    screen.getByRole('heading', { level: 1, name: 'Welcome' })
+  ).toBeDefined();
 });
