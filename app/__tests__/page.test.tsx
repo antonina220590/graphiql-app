a35f54642c0cf1e22a88aefcf820f083a156b51f
Ekaterina Kiryanova <ekaterina.krashennikova@gmail.com>
2024-08-23 17:25:42 +0500

    feat: add buttons for home page

--- app/__tests__/page.test.tsx
+++ app/__tests__/page.test.tsx
@@ -1,11 +1,16 @@
 import { expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 import Page from '../page';
 
-test('Page', () => {
+test('renders the Page component', () => {
   render(<Page />);
   expect(
-    screen.getByRole('heading', { level: 1, name: 'Welcome' })
-  ).toBeDefined();
+    screen.getByRole('heading', { level: 1, name: /welcome!/i })
+  ).toBeInTheDocument();
+
+  expect(screen.getByRole('button', { name: /sign in/i })).toBeInTheDocument();
+
+  expect(screen.getByRole('button', { name: /sign up/i })).toBeInTheDocument();
 });
