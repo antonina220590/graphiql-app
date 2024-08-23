d876bcee534f3da7a6b28491690df71f065163b5
Ekaterina Kiryanova <ekaterina.krashennikova@gmail.com>
2024-08-23 16:33:09 +0500

    fix: change styles for restfullClient page

--- app/restfullClient/page.tsx
+++ app/restfullClient/page.tsx
@@ -1,6 +1,6 @@
 export default function RESTfullClient() {
   return (
-    <main className="flex-grow p-4 bg-[#f1cdb3]">
+    <main className="flex-grow p-4 bg-light">
       <div className="bg-white shadow-md rounded-lg p-6">
         <h1 className="text-xxl font-bold mb-4 text-center w-full">
           REST Client
@@ -15,13 +15,13 @@ export default function RESTfullClient() {
           <input
             type="text"
             placeholder="Endpoint URL"
-            className="border p-2 rounded flex-grow"
+            className="border-2 p-2 rounded flex-grow bg-dark text-white focus:border-yellow-500 focus:outline-none"
           />
         </div>
 
         <div className="mb-4">
           <h2 className="font-semibold">Headers:</h2>
-          <button className="bg-[#fddcc4] text-[#292929] p-2 rounded border hover:border-[#292929] transition duration-300">
+          <button className="bg-[#fe6d12] text-white p-2 rounded border hover:border-[#292929] transition duration-300">
             Add Header
           </button>
         </div>
@@ -30,7 +30,7 @@ export default function RESTfullClient() {
           <h2 className="font-semibold">Body:</h2>
           <textarea
             placeholder="JSON/Text Editor"
-            className="border p-2 rounded w-full h-32"
+            className="border-2 p-2 rounded w-full h-32 bg-dark text-white focus:border-yellow-500 focus:outline-none"
           />
         </div>
       </div>
@@ -38,13 +38,13 @@ export default function RESTfullClient() {
       <div className="font-semibold">Response:</div>
       <div className="flex items-center mb-2">
         <div className="mr-2">Status:</div>
-        <div className="border p-2 rounded bg-gray-100 flex-1">
+        <div className="border p-2 rounded bg-dark flex-1 text-white">
           HTTP Status Code
         </div>
       </div>
       <div className="flex items-center">
         <div className="mr-5">Body:</div>
-        <div className="border p-2 rounded bg-gray-100 flex-1">
+        <div className="border p-2 rounded bg-dark flex-1 text-white">
           Read-Only JSON Viewer
         </div>
       </div>
