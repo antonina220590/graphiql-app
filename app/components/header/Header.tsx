254730e26e5100ddd3ee23f77a6f576e33c5cb32
Ekaterina Kiryanova <ekaterina.krashennikova@gmail.com>
2024-08-28 00:09:24 +0500

    feat: add sticky header

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -4,7 +4,7 @@ import Logo from './logo/logo';
 import LanguageToggler from './langToggler/language';
 export default function Header() {
   return (
-    <header className="bg-dark w-full box-border mx-auto">
+    <header className="bg-dark w-full box-border mx-auto sticky top-0">
       <div className="h-[10vh] flex items-center box-border w-full p-0 px-5 justify-between">
         <Logo />
         <LanguageToggler />
