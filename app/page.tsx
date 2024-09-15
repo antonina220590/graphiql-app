46e8429aa14186ff20393e7ffc34af06a7570985
antonina220590 <tonya2290@gmail.com>
2024-09-15 15:13:15 +0300

    feat: add design to main page

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,9 @@ import { auth } from '../firebaseConfig';
 import { useAuthStatus } from './hooks/useAuthStatus';
 import SignInButton from './components/buttons/SignInButton';
 import SignUpButton from './components/buttons/SignUpButton';
+import PostmanIcon from './components/mainPage/postmanLogo';
+import HandShakeIcon from './components/mainPage/handshake';
+import GraphQLIcon from './components/mainPage/graphLogo';
 
 export default function Page() {
   const { isAuthenticated, checkingStatus, errorMessage } = useAuthStatus();
@@ -28,38 +31,64 @@ export default function Page() {
   }
 
   return (
-    <main className="bg-light flex flex-col items-center justify-center">
-      {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-      {isAuthenticated ? (
-        <div className="text-center">
-          <h1>Welcome Back, {username}!</h1>
-          <div className="flex gap-4 mt-4 justify-center">
-            <Link href="/restfullClient">
-              <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 ease">
-                REST Client
-              </button>
-            </Link>
-            <Link href="/GRAPHQL">
-              <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 ease">
-                GraphiQL Client
-              </button>
-            </Link>
-            <Link href="/history">
-              <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 ease">
-                History
-              </button>
-            </Link>
-          </div>
+    <main className="bg-light flex flex-col items-center  justify-center">
+      <div className="w-3/4 flex flex-col justify-center h-[800px] items-center gap-[100px]">
+        <h4
+          className="text-7xl text-center leading-normal font-bold bg-gradient-to-r from-orange-400 via-red-600 to-blue-600 bg-clip-text text-transparent  animate-gradient-shift"
+          style={{ backgroundSize: '200% 100%' }}
+        >
+          We are happy to greet you!
+        </h4>
+        <span className="text-3xl leading-normal font-bold bg-gradient-to-r from-blue-600 via-red-600 to-orange-400 bg-clip-text text-transparent">
+          This is a light-weight versions of Postman and GrqphiQL combined in
+          one app.
+        </span>
+        <div className="flex items-center gap-[100px]">
+          <GraphQLIcon />
+          <HandShakeIcon />
+          <PostmanIcon />
         </div>
-      ) : (
-        <div className="text-center">
-          <h1>Welcome!</h1>
-          <div className="flex gap-4 mt-4 justify-center">
-            <SignInButton />
-            <SignUpButton />
+      </div>
+      <div className="flex flex-col justify-center h-[800px] items-center">
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        {isAuthenticated ? (
+          <div className="text-center flex flex-col gap-[50px]">
+            <h1 className="text-4xl font-bold bg-gradient-to-r from-red-400 to-blue-600 bg-clip-text text-transparent">
+              Welcome Back, {username}!
+            </h1>
+            <div className="flex gap-4 mt-4 justify-center">
+              <Link href="/restfullClient">
+                <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 ease">
+                  REST Client
+                </button>
+              </Link>
+              <Link href="/GRAPHQL">
+                <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 ease">
+                  GraphiQL Client
+                </button>
+              </Link>
+              <Link href="/history">
+                <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 ease">
+                  History
+                </button>
+              </Link>
+            </div>
           </div>
-        </div>
-      )}
+        ) : (
+          <div className="text-center flex flex-col gap-[30px]">
+            <h1 className="text-5xl font-bold bg-gradient-to-r from-red-400 to-blue-600 bg-clip-text text-transparent">
+              Welcome!
+            </h1>
+            <div className="flex gap-10 mt-4 justify-center">
+              <SignInButton />
+              <SignUpButton />
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="">
+        <span>Our Team</span>
+      </div>
     </main>
   );
 }
