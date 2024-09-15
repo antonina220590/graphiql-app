98ff42cf71fb580c6cebbffd6db7f63c668338ab
Ekaterina Kiryanova <ekaterina.krashennikova@gmail.com>
2024-09-15 20:32:13 +0500

    feat: translate header

--- app/components/buttons/SignUpButton.tsx
+++ app/components/buttons/SignUpButton.tsx
@@ -2,12 +2,14 @@
 
 import Link from 'next/link';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 export default function SignUpButton() {
+  const { t } = useTranslation();
   return (
     <Link href="/signUp">
       <button className="bg-green-900 text-white px-4 py-2 rounded hover:bg-green-950 transition duration-300 ease">
-        Sign Up
+        {t('header.signUp')}
       </button>
     </Link>
   );
